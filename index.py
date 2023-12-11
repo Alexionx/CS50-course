0dafa4cd7e53a0182a850f062e6ac3e9418f30dd
KempachiDZ <alexmelnyk48370@gmail.com>
2023-12-11 13:54:43 +0200

    Create condition

--- index.py
+++ index.py
@@ -2,3 +2,11 @@ print("Hello World")
 
 name = input ("Name: ")
 print(f"Hello, {name} " )
+
+n = int(input("Nubmer: "))
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is zero") 
