fc8c88e5c49c1f121aa8a962e2298397b2dc1dee
KempachiDZ <alexmelnyk48370@gmail.com>
2023-12-11 21:43:53 +0200

    I study Python

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
-print("Hello World")
-
+#print("Hello World")
+'''
 name = input ("Name: ")
 print(f"Hello, {name} " )
+'''
 
+'''condition
 n = int(input("Nubmer: "))
 if n > 0:
     print("n is positive")
@@ -10,3 +12,65 @@ elif n < 0:
     print("n is negative")
 else:
     print("n is zero") 
+'''
+ 
+'''
+cordinateX = 10.0
+cordinateY = 20.0
+
+cordinate = (10.0, 20.0)
+'''
+
+'''
+#Визначив список імен
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+
+names.append("Draco")
+
+names.sort()
+
+print(names)
+'''
+
+'''
+s = set()
+
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+
+s.remove(2)
+print(s)
+
+#Описується сільки елементів в середині множини
+print(f"Кількість елементів {len(s)} в множині")
+'''
+
+'''
+#Цикли в Python
+
+for i in range(6):
+    print(i)
+
+names = ["Harry", "Ron", "Hermione"]
+
+for name in names:
+    print(names)
+'''
+'''
+#Словник
+houses = {"Harry": "Gryffindor", "Draco": "Slytherin"}
+
+houses["Hermiona"] = "Gryffindor"
+
+print(houses["Hermiona"])
+'''
+
+#Function and import dates
+
+def square(x):
+    return x * x
+
