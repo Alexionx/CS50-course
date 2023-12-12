b1da60ee2eb51ad4fda8da3610875fd441facbf0
KempachiDZ <alexmelnyk48370@gmail.com>
2023-12-12 11:29:42 +0200

    Створив програму для авіакомпанії

--- oop.py
+++ oop.py
@@ -10,4 +10,29 @@ print(p.x)
 print(p.y)
 '''
 
+#Програма для відстеження бронювання квитків на рейс і щоб не було переповнення
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+        
+    def add_passenger(self, name):
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+        
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+        
+Flight = Flight(3)
 
+people = ["Harry", "Ron", "Hermione", "Ginny"]
+for person in people:
+    if Flight.add_passenger(person):
+        print(f"Added {person} to flight succesfully")
+    else:
+        print(f"No available seats for {person}")
+        
+
+    
